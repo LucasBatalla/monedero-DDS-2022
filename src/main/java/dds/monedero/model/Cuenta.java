445376617072d835e2864ec4a7b7ccd8007182b8
lbatalla <lbatalla@frba.utn.edu.ar>
2022-05-12 10:00:14 -0300

    Usar variables en vez de getters en metodos extraer y depositar

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -27,7 +27,7 @@ public class Cuenta {
       throw new MontoNegativoException(cantidad + ": el monto a ingresar debe ser un valor positivo");
     }
 
-    if (getMovimientos().stream().filter(movimiento -> movimiento.isDeposito()).count() >= 3) {
+    if (movimientos.stream().filter(movimiento -> movimiento.isDeposito()).count() >= 3) {
       throw new MaximaCantidadDepositosException("Ya excedio los " + 3 + " depositos diarios");
     }
 
@@ -38,8 +38,8 @@ public class Cuenta {
     if (cantidad <= 0) {
       throw new MontoNegativoException(cantidad + ": el monto a ingresar debe ser un valor positivo");
     }
-    if (getSaldo() - cantidad < 0) {
-      throw new SaldoMenorException("No puede sacar mas de " + getSaldo() + " $");
+    if (saldo - cantidad < 0) {
+      throw new SaldoMenorException("No puede sacar mas de " + saldo + " $");
     }
     double montoExtraidoHoy = getMontoExtraidoA(LocalDate.now());
     double limite = 1000 - montoExtraidoHoy;
